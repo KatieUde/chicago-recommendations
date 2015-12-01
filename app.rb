0bec21213e74220a69783f27dc6c61fd2cf06936
Katherine Ude <Katie.Ude@gmail.com>
2015-12-01 12:38:55 -0600

    saving more class work progress

--- app.rb
+++ app.rb
@@ -2,10 +2,15 @@ require 'bundler'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(
-  :adapter => 'postgresql'
+  :adapter => 'postgresql',
   :database => 'chicago_places'
 )
 
 get '/' do
   return 'Test...123'
+end
+
+get '/api' do
+  # SELECT * FROM recommendations;
+  Recommendation.all.to_json
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require './app.rb'
+require './models/recommendation'
 
 run Sinatra::Application
